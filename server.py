1a523fb8439898da2b6f69bbfc618ca86e1661ce
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-12-22 13:49:16 +0300

    Add livereload

---
+++ server.py
@@ -0,0 +1,10 @@
+from livereload import Server, shell
+def rebuild():
+    ... # do things
+    print("Site rebuilt")
+
+rebuild()
+
+server = Server()
+server.watch('index.html', rebuild)
+server.serve(root='.')
